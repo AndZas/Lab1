8dcb794888d241ddbc1d8763a815b4df80214f02
Andrey Zasimov <zasimovandrey2000@gmail.com>
2026-03-18 15:32:08 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import numpy as np
-from scipy.stats import lognorm
+from scipy import stats
 
-data = np.array([
-    814, 491, 346, 310, 211, 151, 125, 89, 73, 71, 65, 58, 57, 54, 48, 46, 45, 37, 33, 28, 27, 26, 24, 23, 22, 22, 22, 19, 18
-])
+data = np.array([814, 491, 346, 310, 211, 151, 125, 89, 73, 71, 65, 58, 57, 54, 48, 46, 45, 37, 33, 28, 27, 26, 24, 23, 22, 22, 22, 19, 18])
 
-shape, loc, scale = lognorm.fit(data)
+# --- ЛОГНОРМАЛЬНОЕ РАСПРЕДЕЛЕНИЕ ---
+shape, loc, scale = stats.lognorm.fit(data)
 
-print("Параметры логнормального распределения:")
+print("Lognormal params:")
 print("shape =", shape)
 print("loc =", loc)
 print("scale =", scale)
+
+# --- KS тест ---
+ks_stat, p_value = stats.kstest(data, 'lognorm', args=(shape, loc, scale))
+
+print("KS statistic:", ks_stat)
+print("p-value:", p_value)
+
+# --- ОСНОВНЫЕ ХАРАКТЕРИСТИКИ ---
+mean = np.mean(data)
+std = np.std(data, ddof=1)
+
+print("Mean:", mean)
+print("Std:", std)
